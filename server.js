d8afd9b5a155bc235126328a8a2acc26aafac403
ANISH BISWAS <iammanishbiswas@gmail.com>
2024-03-02 02:06:38 +0530

    Final eval

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const cors = require('cors');
 
 app.use(cors());
 app.use(express.json());
-app.use("/api/v1/auth" , authRoutes);
+app.use("/api/v1/auth" , authRoutes);//https://todo-backend-mza6.onrender.com
 app.use("/api/v1/todo",todoRoutes);
 
 app.get("/health" , (req,res)=>{
